74ce48c6adb9d8db96ba155c9669bd4625298239
Taylor Ashton <taylor.ashton17@gmail.com>
2016-04-09 08:46:49 -0500

    added number 7

--- queries.js
+++ queries.js
@@ -1,7 +1,7 @@
 db.bios.find({awards:{$exists:true} })
 db.bios.find({awards:{$exists:false} })
- db.bios.find({$or: [{contribs: "OOP"}, {contribs: "UNIX"}]}).pretty()
+db.bios.find({$or: [{contribs: "OOP"}, {contribs: "UNIX"}]}).pretty()
 db.bios.find({awards:{$elemMatch:{award:"Turing Award"}}}).pretty()
-.bios.find({$and: [{_id:{$lt:7}}, {_id:{$gt:3}}]}).pretty()
-
-    db.bios.find({$and: [{birth:{$exists:true} }, {death:{$exists:false} }]}).pretty()
+db.bios.find({$and: [{_id:{$lt:7}}, {_id:{$gt:3}}]}).pretty()
+db.bios.find({awards:{$elemMatch:{year:{$lt: 2000}}}}).pretty()
+db.bios.find({$and: [{birth:{$exists:true} }, {death:{$exists:false} }]}).pretty()
